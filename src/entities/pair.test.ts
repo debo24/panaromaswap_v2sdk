9aaae9910a3613b0f552dcd1dc2e2c6c35294409
debo24 <71261693+debo24@users.noreply.github.com>
2022-07-25 19:20:54 +0530

    Update pair.test.ts

--- src/entities/pair.test.ts
+++ src/entities/pair.test.ts
@@ -1,4 +1,4 @@
-import { Token, WETH9, Price, CurrencyAmount } from '@panaromaswap/sdk-core'
+import { Token, WETH9, Price, CurrencyAmount } from 'panaromaswap_sdkcore'
 import { InsufficientInputAmountError } from '../errors'
 import { computePairAddress, Pair } from './pair'
 
